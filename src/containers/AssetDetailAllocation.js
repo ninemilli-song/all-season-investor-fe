41c72b67c16784e472dc8dca4f25cd6e5bdc76be
ninemilli.song <ninemilli_song@163.com>
2018-11-14 18:38:19 +0800

    资产详情页面添加代码

---
+++ src/containers/AssetDetailAllocation.js
@@ -0,0 +1,18 @@
+/**
+ * 资产配置组件
+ */
+import React from 'react';
+import { observer } from 'mobx-react';
+
+@observer
+class AssetDetailAllocation extends React.Component {
+    render() {
+        return (
+            <div>
+                AssetDetailAllocation
+            </div>
+        );
+    }
+}
+
+export default AssetDetailAllocation;
--- src/pages/assetDetail.js
+++ src/pages/assetDetail.js
@@ -4,6 +4,10 @@ import {
 } from 'react-intl';
 import Layout from '../components/Layout.js';
 import pageWithIntl from '../components/PageWithIntl.js';
+import AssetDetailAllocation from '../containers/AssetDetailAllocation';
+import AssetDetailCategoryRate from '../containers/AssetDetailCategoryRate';
+import AssetDetailSafeRate from '../containers/AssetDetailSafeRate';
+import AssetDetailAnalysis from '../containers/AssetDetailAnalysis';
 
 class AssetDetail extends React.Component {
     static async getInitialProps({ req }) {
@@ -12,10 +16,51 @@ class AssetDetail extends React.Component {
         };
     }
 
+    prefixCls = 'asset-detail'
+
     render() {
         return (
-            <Layout title="All season investor">
-                <FormattedMessage id="assetDetailPage" defaultMessage="Hello AssetDetail!" />
+            <Layout title="Asset Analysis">
+                <div className={`${this.prefixCls}-header`}>
+                    <h2>
+                        <FormattedMessage id="pageTitle" defaultMessage="Asset Analysis" />
+                    </h2>
+                    <span>
+                        {'资产配置以及资产分析'}
+                    </span>
+                </div>
+                <div className={`${this.prefixCls}-allocation`}>
+                    <h3>
+                        {'资产配置'}
+                    </h3>
+                    <div>
+                        <AssetDetailAllocation />
+                    </div>
+                </div>
+                <div className={`${this.prefixCls}-category-rate`}>
+                    <h3>
+                        {'资产类型比例'}
+                    </h3>
+                    <div>
+                        <AssetDetailCategoryRate />
+                    </div>
+                </div>
+                <div className={`${this.prefixCls}-safe-rate`}>
+                    <h3>
+                        {'安全类别比例'}
+                    </h3>
+                    <div>
+                        <AssetDetailSafeRate />
+                    </div>
+                </div>
+                <div className={`${this.prefixCls}-analysis`}>
+                    <h3>
+                        {'资产配置汇总分析'}
+                    </h3>
+                    <div>
+                        <AssetDetailAnalysis />
+                    </div>
+                </div>
             </Layout>
         );
     }
