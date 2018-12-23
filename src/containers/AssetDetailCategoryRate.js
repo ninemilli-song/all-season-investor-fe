ad31c330b2cfb79beb380d5311db6b0c7bbad224
ninemilli.song <ninemilli_song@163.com>
2018-12-23 19:59:07 +0800

    思考数据格式

--- src/containers/AssetDetailCategoryRate.js
+++ src/containers/AssetDetailCategoryRate.js
@@ -4,6 +4,24 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
+// Data struct defind
+// type AssetAnalysis = {
+//     category: {
+//         id: number;
+//         name: string;
+//         code: string;
+//     };
+//     amount: number;
+//     suggestAmount: number;
+//     rate: number;
+//     suggestRate: number;
+//     descripte: string;
+//     analysis: string;
+//     assets: Array<asset>;
+// }
+
+// Array<AssetAnalysis>
+
 @observer
 class AssetDetailCategoryRate extends React.Component {
     render() {
