6a3a314c0fc98939f9de4d9e2d972f370597a7f4
ninemilli.song <ninemilli_song@163.com>
2019-01-27 14:22:38 +0800

    1. 调整eslint 2. 调整资产form组件逻辑

--- src/containers/asset-details/AssetDetails.jsx
+++ src/containers/asset-details/AssetDetails.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import {
-    Table, Divider, Icon, Popconfirm, Button 
+  Table, Divider, Icon, Popconfirm, Button 
 } from 'antd';
 import { loggedIn } from '../../util/AuthService';
 import LoadingIcon from '../../components/LoadingIcon';
@@ -15,237 +15,256 @@ import AssetDialog, { AssetDialogAction } from './AssetDialog';
 @inject('assets', 'investorId')
 @observer
 class AssetDetails extends React.Component {
-    prefix = 'asset-detail-allocation'
+  prefix = 'asset-detail-allocation';
 
-    // 表列定义
-    columnsDef = [
-        {
-            title: '序号',
-            dataIndex: 'id',
-            key: 'type.code',
-            render: (text, row, index) => (
-                <span>
-                    { (this.currentPage - 1) * this.paginationSize + index + 1 }
-                </span>
-            )
-        },
-        {
-            title: '名称',
-            dataIndex: 'type.name',
-            key: 'type.name',
-        },
-        {
-            title: '类型',
-            dataIndex: 'type.type.name',
-            key: 'type.type.name',
-        },
-        {
-            title: '安全类别',
-            dataIndex: 'type.type.bucket.name',
-            key: 'type.type.bucket.name',
-        },
-        {
-            title: '资产',
-            dataIndex: 'amount',
-            key: 'amount',
-            align: 'right',
-            width: 200,
-            editable: true
-        },
-        {
-            title: '操作',
-            align: 'center',
-            render: data => (
-                <div>
-                    <a data-id={data.id} onClick={this.onEdit} role="presentation">
-                        <Icon type="edit" />
-                    </a>
-                    <Divider type="vertical" />
-                    <a>
-                        <Popconfirm 
-                            title="亲！确定删除此资产么？" 
-                            icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
-                            data-data={data.id}
-                            onConfirm={() => { this.onDelete(data.id); }}
-                        >
-                            <Icon type="delete" />
-                        </Popconfirm>
-                    </a>
-                </div>
-            )
-        }
-    ];
-
-    paginationSize = 5;
-
-    currentPage = 1;
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            assetDialogOpts: {
-                visible: false,
-                action: AssetDialogAction.NEW,
-                data: null
-            }
-        };
+  // 表列定义
+  columnsDef = [
+    {
+      title: '序号',
+      dataIndex: 'id',
+      key: 'type.code',
+      render: (text, row, index) => (
+        <span>
+          { (this.currentPage - 1) * this.paginationSize + index + 1 }
+        </span>
+      )
+    },
+    {
+      title: '名称',
+      dataIndex: 'type.name',
+      key: 'type.name',
+    },
+    {
+      title: '类型',
+      dataIndex: 'type.type.name',
+      key: 'type.type.name',
+    },
+    {
+      title: '安全类别',
+      dataIndex: 'type.type.bucket.name',
+      key: 'type.type.bucket.name',
+    },
+    {
+      title: '资产',
+      dataIndex: 'amount',
+      key: 'amount',
+      align: 'right',
+      width: 200,
+      editable: true
+    },
+    {
+      title: '操作',
+      align: 'center',
+      render: data => (
+        <div>
+          <a data-id={data.id} onClick={this.onEdit} role="presentation">
+            <Icon type="edit" />
+          </a>
+          <Divider type="vertical" />
+          <a>
+            <Popconfirm
+              title="亲！确定删除此资产么？"
+              icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
+              data-data={data.id}
+              onConfirm={() => { this.onDelete(data.id); }}
+            >
+              <Icon type="delete" />
+            </Popconfirm>
+          </a>
+        </div>
+      )
     }
+  ];
 
-    componentDidMount() {
-        const { assets, investorId } = this.props;
+  paginationSize = 5;
 
-        // load data
-        assets.fetchAssets(investorId);
-    }
+  currentPage = 1;
 
-    get totalAmount() {
-        const { assets } = this.props;
+  // form对象引用
+  formRef = null;
 
-        return assets.assetsData.reduce((accumultor, currentValue) => {
-            return accumultor + currentValue.amount;
-        }, 0);
-    }
+  constructor(props, context) {
+    super(props, context);
 
-    handleSave = (row) => {
-        const { assets } = this.props;
-        const { id, amount } = row;
+    this.state = {
+      assetDialogOpts: {
+        visible: false,
+        action: AssetDialogAction.NEW,
+        data: null
+      }
+    };
+  }
 
-        assets.updateAsset(id, parseFloat(amount));
-    }
+  componentDidMount() {
+    const { assets, investorId } = this.props;
 
-    /**
-     * 新增资产
-     */
-    onAdd = () => {
-        this.setState({
-            assetDialogOpts: {
-                visible: true,
-                action: AssetDialogAction.NEW,
-                data: null
-            }
-        });
-    }
+    // load data
+    assets.fetchAssets(investorId);
+  }
 
-    /**
-     * 编辑资产
-     */
-    onEdit = (e) => {
-        const { assets } = this.props;
-        const { assetsData } = assets;
-        const { id } = e.currentTarget.dataset;
-        
-        const data = assetsData.find(item => item.id === Number(id));
+  get totalAmount() {
+    const { assets } = this.props;
 
-        this.setState({
-            assetDialogOpts: {
-                visible: true,
-                action: AssetDialogAction.EDIT,
-                data
-            }
-        });
-    }
+    return assets.assetsData.reduce((accumultor, currentValue) => {
+      return accumultor + currentValue.amount;
+    }, 0);
+  }
 
-    onCloseDialog = () => {
-        this.closeDialog();
-    }
+  handleSave = (row) => {
+    const { assets } = this.props;
+    const { id, amount } = row;
 
-    onDialogSubmit = () => {
-        this.closeDialog();
-    }
+    assets.updateAsset(id, parseFloat(amount));
+  }
+
+  /**
+   * 新增资产
+   */
+  onAdd = () => {
+    this.setState({
+      assetDialogOpts: {
+        visible: true,
+        action: AssetDialogAction.NEW,
+        data: null
+      }
+    });
+  }
+
+  /**
+   * 编辑资产
+   */
+  onEdit = (e) => {
+    const { assets } = this.props;
+    const { assetsData } = assets;
+    const { id } = e.currentTarget.dataset;
+
+    const data = assetsData.find(item => item.id === Number(id));
+
+    this.setState({
+      assetDialogOpts: {
+        visible: true,
+        action: AssetDialogAction.EDIT,
+        data
+      }
+    });
+  };
+
+  onCloseDialog = () => {
+    this.closeDialog();
+  };
+
+  onDialogSubmit = () => {
+    const { form } = this.formRef.props;
+
+    form.validateFields((error, values) => {
+      if (error) {
+        return;
+      }
+
+      console.log('form submit value: ', values);
+    });
+  };
 
   /**
    * 关闭对话框
    */
   closeDialog = () => {
-      this.setState({
-          assetDialogOpts: {
-              visible: false,
-              action: AssetDialogAction.NEW,
-              data: null
-          }
-      });
+    this.setState({
+      assetDialogOpts: {
+        visible: false,
+        action: AssetDialogAction.NEW,
+        data: null
+      }
+    });
   };
 
     /**
      * 删除资产
      */
     onDelete = (id) => {
-        console.log('onDelete id: ', id);
-    }
+      console.log('onDelete id: ', id);
+    };
 
-    render() {
-        const { assets } = this.props;
-        const { assetDialogOpts } = this.state;
+  /**
+   * 获取Form对象引用
+   */
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  };
 
-        const { assetsData } = assets;
+  render() {
+    const { assets } = this.props;
+    const { assetDialogOpts } = this.state;
 
-        const components = {
-            body: {
-                row: TableEditableFormRow,
-                cell: TableEditableCell
-            }
-        };
+    const { assetsData } = assets;
 
-        const columnsDef = this.columnsDef.map((col) => {
-            if (!col.editable) {
-                return col;
-            }
-            return {
-                ...col,
-                onCell: record => ({
-                    record,
-                    editable: loggedIn() ? col.editable : false,
-                    dataIndex: col.dataIndex,
-                    title: col.title,
-                    handleSave: this.handleSave
-                })
-            };
-        });
+    const components = {
+      body: {
+        row: TableEditableFormRow,
+        cell: TableEditableCell
+      }
+    };
 
-        const loading = !(assetsData.length > 0);
+    const columnsDef = this.columnsDef.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          editable: loggedIn() ? col.editable : false,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave
+        })
+      };
+    });
 
-        return (
-            <div className={`${this.prefix}`}>
-                <h3>
-                    {'资产配置'}
-                </h3>
-                <div className={`${this.prefix}-header clearfix`}>
-                    <span className={`${this.prefix}-totalAmount`}>
-                        {`总资产 -- ${this.totalAmount}`}
-                    </span>
-                    <div className={`${this.prefix}-operator floatRight`}>
-                        <Button type="primary" icon="plus" onClick={this.onAdd}>添加新资产</Button>
-                    </div>
-                </div>
-                <div>
-                    <Table 
-                        components={components}
-                        rowClassName={() => 'editable-row'}
-                        dataSource={assetsData} 
-                        columns={columnsDef}
-                        size="small"
-                        // scroll={{ x: 500, y: 500 }}
-                        bordered
-                        loading={loading ? { indicator: LoadingIcon() } : false}
-                        pagination={{
-                            pageSize: this.paginationSize,
-                            // size: 'small'
-                        }}
-                        onChange={(pagination) => {
-                            const { current } = pagination;
-                            this.currentPage = current;
-                        }}
-                    />
-                </div>
-                <AssetDialog
-                    {...assetDialogOpts}
-                    onCancel={this.onCloseDialog}
-                    onOk={this.onDialogSubmit}
-                />
-            </div>
-        );
-    }
+    const loading = !(assetsData.length > 0);
+
+    return (
+      <div className={`${this.prefix}`}>
+        <h3>
+          {'资产配置'}
+        </h3>
+        <div className={`${this.prefix}-header clearfix`}>
+          <span className={`${this.prefix}-totalAmount`}>
+            {`总资产 -- ${this.totalAmount}`}
+          </span>
+          <div className={`${this.prefix}-operator floatRight`}>
+            <Button type="primary" icon="plus" onClick={this.onAdd}>添加新资产</Button>
+          </div>
+        </div>
+        <div>
+          <Table 
+            components={components}
+            rowClassName={() => 'editable-row'}
+            dataSource={assetsData} 
+            columns={columnsDef}
+            size="small"
+            // scroll={{ x: 500, y: 500 }}
+            bordered
+            loading={loading ? { indicator: LoadingIcon() } : false}
+            pagination={{
+              pageSize: this.paginationSize,
+              // size: 'small'
+            }}
+            onChange={(pagination) => {
+              const { current } = pagination;
+              this.currentPage = current;
+            }}
+          />
+        </div>
+        <AssetDialog
+          {...assetDialogOpts}
+          wrappedComponentRef={this.saveFormRef}
+          onCancel={this.onCloseDialog}
+          onOk={this.onDialogSubmit}
+        />
+      </div>
+    );
+  }
 }
 
 export default AssetDetails;
--- src/containers/asset-details/AssetDialog.jsx
+++ src/containers/asset-details/AssetDialog.jsx
@@ -3,54 +3,125 @@
  * 用于新增、编辑资产
  */
 import React from 'react';
-import { Modal } from 'antd';
+import { Modal, Form, Input } from 'antd';
 import PropTypes from 'prop-types';
-import AssetForm from './AssetForm';
 
 export const AssetDialogAction = {
-    NEW: 'new',
-    EDIT: 'edit'
+  NEW: 'new',
+  EDIT: 'edit'
 };
 
+@Form.create({ name: 'asset_dialog' })
 class AssetDialog extends React.Component {
-    prefixCls = 'asi-asset-dialog';
+  prefixCls = 'asi-asset-dialog';
 
-    static defaultProps = {
-        action: AssetDialogAction.NEW,
-        data: {},
-        loading: false,
-        visible: false
-    }
+  static defaultProps = {
+    action: AssetDialogAction.NEW,
+    data: {},
+    loading: false,
+    visible: false,
+  };
 
-    render() {
-        const { 
-            action,
-            data,
-            loading,
-            ...otherProps
-        } = this.props;
+  formItemLayout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 14 },
+  };
 
-        return (
-            <div className={`${this.prefixCls}`}>
-                <Modal
-                    title={action === AssetDialogAction.NEW ? '新增资产' : '编辑资产'}
-                    confirmLoading={loading}
-                    {...otherProps}
-                >
-                    <AssetForm
-                        data={data}
-                    />
-                </Modal>
-            </div>
-        );
-    }
+  saveFormRef = (ref) => {
+    this.formRef = ref;
+  };
+
+  render() {
+    const { 
+      action,
+      data,
+      loading,
+      form,
+      ...otherProps
+    } = this.props;
+
+    const { getFieldDecorator } = form;
+
+    return (
+      <div className={`${this.prefixCls}`}>
+        <Modal
+          title={action === AssetDialogAction.NEW ? '新增资产' : '编辑资产'}
+          confirmLoading={loading}
+          {...otherProps}
+        >
+          <Form>
+            <Form.Item
+              {...this.formItemLayout}
+              label="资产品种名称"
+            >
+              {
+                getFieldDecorator('name', {
+                  rules: [{ required: true, message: '亲，请输入资产品种名称!' }],
+                  initialValue: (data && data.type) ? data.type.name : ''
+                })(
+                  <Input
+                    placeholder="资产品种名称"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item
+              {...this.formItemLayout}
+              label="资产品种代码"
+            >
+              {
+                getFieldDecorator('code', {
+                  rules: [{ required: true, message: '亲，请输入资产品种代码!' }],
+                  initialValue: (data && data.type) ? data.type.code : ''
+                })(
+                  <Input
+                    placeholder="资产品种代码"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item
+              {...this.formItemLayout}
+              label="资产类型"
+            >
+              {
+                getFieldDecorator('category', {
+                  rules: [{ required: true, message: '亲，请选择对应的资产类型!' }],
+                  initialValue: (data && data.type) ? data.type.category.name : ''
+                })(
+                  <Input
+                    placeholder="资产类型"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item
+              {...this.formItemLayout}
+              label="当前金额"
+            >
+              {
+                getFieldDecorator('amount', {
+                  rules: [{ required: true, message: '亲，请该资产当前金额!' }],
+                  initialValue: data ? data.amount : ''
+                })(
+                  <Input
+                    placeholder="资产类型"
+                  />
+                )
+              }
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 AssetDialog.propTypes = {
-    action: PropTypes.string, // 'new'|'edit' 
-    data: PropTypes.object,
-    loading: PropTypes.bool,
-    visible: PropTypes.bool
+  action: PropTypes.string, // 'new'|'edit' 
+  data: PropTypes.object,
+  loading: PropTypes.bool,
+  visible: PropTypes.bool
 };
 
 export default AssetDialog;
