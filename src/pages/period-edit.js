63c3b38a29bd983f7be26c0621222f24010eba0f
ninemilli.song <ninemilli_song@163.com>
2019-08-07 20:05:21 +0800

    期初录入页面“”

--- src/pages/period-edit.js
+++ src/pages/period-edit.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {
+    Form, InputNumber, DatePicker, Button 
+} from 'antd';
+
+const FormItem = Form.Item;
 
 function PeriodEdit() {
     return (
         <div>
-            Hello PeriodEdit!
+            <Form layout="horizontal">
+                <FormItem
+                    label="基金代码"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 8 }}
+                >
+                    <InputNumber 
+                        size="large" 
+                        style={{ width: 200 }} 
+                        formatter={value => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        precision={2}
+                        name="inputNumber" 
+                    />
+                </FormItem>
+                <FormItem
+                    label="起始时间"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 8 }}
+                >
+                    <DatePicker name="startDate" />
+                </FormItem>
+                <FormItem
+                    style={{ marginTop: 48 }}
+                    wrapperCol={{ span: 8, offset: 8 }}
+                >
+                    <Button size="large" type="primary" htmlType="submit">
+                        OK
+                    </Button>
+                    <Button size="large" style={{ marginLeft: 8 }}>
+                        Cancel
+                    </Button>
+                </FormItem>
+            </Form>
         </div>
     );
 }
